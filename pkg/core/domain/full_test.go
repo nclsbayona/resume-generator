085da6c959c9dbe53b7186b4ba690c15348ced8e
nclsbayona <nbayonaord@gmail.com>
2024-06-22 21:27:50 -0500

    test: :white_check_mark: Add tests for full resume

---
+++ pkg/core/domain/full_test.go
@@ -0,0 +1,73 @@
+package domain
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestNewFullResume(t *testing.T) {
+	type args struct {
+		name        *string
+		template    *string
+		experiences []*Experience
+		education   []*Education
+	}
+	type test struct {
+		name *string
+		args *args
+		want *FullResume
+	}
+
+	var (
+		name      = "Test Full NewFullResume"
+		arguments = args{
+			name:     &name,
+			template: &name,
+			experiences: []*Experience{
+				{
+					Title:        &name,
+					Company:      &name,
+					Location:     &name,
+					StartDate:    &name,
+					EndDate:      &name,
+					Description:  &name,
+					Achievements: []*sAchievement{stringAchievement(&name)},
+				},
+			},
+			education: []*Education{
+				{
+					Title:       &name,
+					Institution: &name,
+					Location:    &name,
+					StartDate:   &name,
+					EndDate:     &name,
+					Description: &name,
+				},
+			},
+		}
+		want = FullResume{
+			Name:        arguments.name,
+			Template:    arguments.template,
+			Experiences: arguments.experiences,
+			Education:   arguments.education,
+		}
+	)
+	testCase := test{
+		name: &name,
+		args: &arguments,
+		want: &want,
+	}
+	t.Run(*testCase.name, func(t *testing.T) {
+		if got := NewFullResume(testCase.args.name, testCase.args.template, testCase.args.experiences, testCase.args.education); !reflect.DeepEqual(got, testCase.want) {
+			t.Errorf("NewFullResume() = %v, want %v", got, testCase.want)
+		}
+	})
+	name = "Test Empty NewFullResume"
+	arguments = args{}
+	want = FullResume{}
+	t.Run(*testCase.name, func(t *testing.T) {
+		if got := NewFullResume(testCase.args.name, testCase.args.template, testCase.args.experiences, testCase.args.education); !reflect.DeepEqual(got, testCase.want) {
+			t.Errorf("NewFullResume() = %v, want %v", got, testCase.want)
+		}
+	})
+}
